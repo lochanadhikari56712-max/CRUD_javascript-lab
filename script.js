12a8c0d617604a92873efb6d175fb74f32d2a0f2
Lochan Adhikari <lochanadhikari56712@gmail.com>
2026-03-13 07:48:03 +0545

    added script

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let students = []
-let editIndex = null
+let students = [];
+let editIndex = null;
 function addStudent() {
   let name = document.getElementById("nameInput").value;
 
@@ -7,18 +7,35 @@ function addStudent() {
     alert("Please enter a name");
     return;
   }
-}
-if (editIndex === null) {
-  let student = {
-    id: students.length + 1,
-    name: name
-  }
-  students.push(student);
-} else {
-  students[editIndex].name = name;
-  editIndex = null;
-}
-document.getElementById("nameInput").value = "";
-showStudents()
-}
 
+  if (editIndex === null) {
+    let student = {
+      id: students.length + 1,
+      name: name,
+    };
+    students.push(student);
+  } else {
+    students[editIndex].name = name;
+    editIndex = null;
+  }
+  document.getElementById("nameInput").value = "";
+  showStudents();
+}
+function showStudents() {
+  let table = document.getElementById("tableBody");
+
+  table.innerHTML = "";
+
+  for (let i = 0; i < students.length; i++) {
+    let row = "<tr>";
+    row += "<td>" + students[i].id + "</td>";
+    row += "<td>" + students[i].name + "</td>";
+    row += "<td>";
+    row += "<button onclick='editStudent(" + i + ")'>Edit</button>";
+    row += "<button onclick='deleteStudent(" + i + ")'>Delete</button>";
+    row += "</td>";
+    row += "</tr>";
+
+    table.innerHTML += row;
+  }
+}
